ccb6d4dca1a41d278d84d82c4b1796f69c539b41
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2023-03-31 12:58:00 +0300

    Update

--- models/Restaurants.js
+++ models/Restaurants.js
@@ -21,12 +21,9 @@ exports.validRestaurants = (_bodyData) => {
     let joiSchema = Joi.object({
         name: Joi.string().min(2).max(99).required(),
         type: Joi.string().min(2).max(99).required(),
-        tags: Joi.string().min(2).max(99),
         location: Joi.string().min(2).max(99),
         distance: Joi.number().min(2).max(99).required(),
         time: Joi.number().min(2).max(9999).required(),
-        images: Joi.string().min(2).max(300),
-        categories: Joi.string.min(2).max(99)
     })
 
     return joiSchema.validate(_bodyData);
