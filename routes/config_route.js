6c6084c352138de26a38ce9497488d3fc7ebd774
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2023-04-11 18:43:28 +0300

    Update

--- routes/config_route.js
+++ routes/config_route.js
@@ -1,11 +1,9 @@
 const indexS = require("./users");
 const indexX = require("./index");
-const foodsX = require("./foods");
 const restaurantsX = require("./restaurants");
 
 exports.routesInit = (app) => {
     app.use("/restaurants", restaurantsX),
         app.use("/users", indexS),
-        app.use("/", indexX),
-        app.use("/foods", foodsX)
+        app.use("/", indexX)
 }
